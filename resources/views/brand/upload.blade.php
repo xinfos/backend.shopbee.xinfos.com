266fe2ad68d5728fd8359bc84529cc6f06b09aec
Alex Pan <psj474@gmail.com>
2020-11-04 16:59:44 +0800

    feature -- add upload

--- resources/views/brand/upload.blade.php
+++ resources/views/brand/upload.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.dashkit.app')
 @section('content')
 
+<link rel="stylesheet" type="text/css" href="/assets/dashkit/libs/upload/dist/sf.upload.css">
 <div class="row justify-content-center content" style="margin:0px;background-color:#f7f8fa;">
     <div class="sf-product-frm col-12 col-lg-10 col-xl-10">
         <!-- Form -->
@@ -24,138 +25,88 @@
             </div>
         </form>
         <div class="sf-product-push">
-            <div class="sf-product-push-body">
-                <button class="sf-btn sf-btn-primary" data-toggle="modal" data-target="#modalCreateCategory">新建分类</button>
-            </div>
         </div>
     </div>
 </div>
+<button type="button" class="sf-btn sf-btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+    开始
+</button>
 
-<div class="modal fade" tabindex="-1" id="modalCreateCategory" role="dialog" aria-hidden="true" style="width: 900px; max-width: 1000px;">
-    <div class="modal-dialog" role="document">
+<div class="modal fade show" id="exampleModalLong" tabindex="-1" aria-modal="true" role="dialog">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title">图片空间
-                    <small class="text-muted">（图片要求：宽高比为1:1，宽高大于480px，单张图片小于3MB）</small>
-                    <small><a href="#">示例图片</a></small>
-                </h4>
-            </div>
-            <div class="MDL_body_4-64-0" style="padding: 12px 20px;">
-                <div class="BeastMaterialModal___content___13wyK">
-                    <div class="BeastMaterialModal___tab-wrapper___2v0DO">
-                        <div class="BeastMaterialModal___tab___1dQI9">
-                            <div class="BeastShowTypeTab___tab-list___1aBAA">
-                                <i data-testid="beast-core-icon-all" class="ICN_outerWrapper_4-64-0 ICN_type-all_4-64-0 " style="font-size: 16px; color: rgb(17, 153, 238);">
-                                </i>
-                                <i data-testid="beast-core-icon-list" class="ICN_outerWrapper_4-64-0 ICN_type-list_4-64-0 " style="font-size: 16px; color: rgb(102, 102, 102); margin: 0px 24px 0px 12px;">
-                                </i>
-                            </div>
-                            <div class="BeastMaterialModal___search___1pW_I">
-                                <div data-testid="beast-core-input" class="IPT_outerWrapper_4-64-0 IPT_medium_4-64-0">
-                                    <div class="IPT_reunitBlock_4-64-0">
-                                        <div class="IPT_inputWrapper_4-64-0">
-                                            <div class="IPT_inputBlock_4-64-0">
-                                                <div class="IPT_inputBlockCell_4-64-0">
-                                                    <input data-testid="beast-core-input-htmlInput" placeholder="请输入图片名称" type="text" class="IPT_input_4-64-0" value="1" data-track-logtag="tagged">
-                                                </div>
-                                                <div class="IPT_suffixCell_4-64-0 IPT_prefixSuffixCell_4-64-0 IPT_pointerCell_4-64-0" data-testid="beast-core-input-suffix">
-                                                    <div class="IPT_suffixWrapper_4-64-0 IPT_divide_4-64-0">
-                                                        <div>
-                                                            查询
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="BeastMaterialModal___reload___1LPuT">
-                                刷新
-                            </div>
-                            <label class="CBX_outerWrapper_4-64-0 CBX_checkbox_4-64-0 CBX_medium_4-64-0" data-testid="beast-core-checkbox" data-checked="false" data-indeterminate="false" style="margin-left: 12px;">
-                                <div class="CBX_squareInputWrapper_4-64-0" data-testid="beast-core-checkbox-checkIcon">
-                                    <input mode="checkbox" class="CBX_input_4-64-0" type="checkbox" data-track-logtag="tagged">
-                                    <div class="CBX_square_4-64-0 CBX_groupDisabled_4-64-0 CBX_hasCheckSquare_4-64-0">
-                                        <i data-testid="beast-core-icon-check" class="ICN_outerWrapper_4-64-0 ICN_type-check_4-64-0  CBX_iconCheck_4-64-0">
-                                        </i>
-                                    </div>
-                                </div>
-                                <div class="CBX_textWrapper_4-64-0 CBX_prevCheckSquare_4-64-0">
-                                    仅展示可用图片
-                                </div>
-                            </label>
+            <div class="modal-card card" style="border-radius: 15px;">
+                <div class="card-header">
+                    <h4 class="card-header-title" id="exampleModalCenterTitle">
+                        图片上传
+                        <small class="text-muted">（图片要求：宽高比为1:1，宽高大于480px，单张图片小于3MB）</small>
+                        <small class="text-muted"><a href="javascript:void(0);">示例图</a></small>
+                    </h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="card-body">
+                    <div class="sf-upload-header">
+                        <div class="sf-upload-tab">
+                            <div><i class="fe fe-grid i-active"></i></div>
+                            <div class="ml-2"><i class="fe fe-list"></i></div>
                         </div>
-                        <div class="BeastMaterialModal___upload-btn___X8rBK">
-                            <button class="BTN_outerWrapper_4-64-0 BTN_gray_4-64-0 BTN_medium_4-64-0 BTN_outerWrapperBtn_4-64-0" type="button" data-testid="beast-core-button">
-                                <i data-testid="beast-core-icon-upload" class="ICN_outerWrapper_4-64-0 ICN_type-upload_4-64-0 " style="font-size: 14px; margin-right: 8px;">
-                                </i>
-                                <span>
-                                    本地上传
-                                </span>
-                            </button>
+                        <div class="sf-upload-tab ml-4">
+                            <div class="input-group">
+                                <input type="text" class="sf-input-sm" placeholder="请输入图片名称" name="images">
+                            </div>
+                        </div>
+                        <div class="sf-upload-tab ml-1">
+                            <a class="sf-btn sf-btn-primary sf-upload-search-btn" href="javascript:void(0);">查询</a>
+                        </div>
+                        <div class="sf-upload-tab sf-upload-fresh">
+                            <span class=""><a href="javascript:void(0);">刷新</a></span>
+                        </div>
+                        <div class="sf-upload-tab sf-upload-local">
+                            <button class="sf-btn sf-btn-upload"><i class="fe fe-upload"></i> 本地上传</button>
                         </div>
                     </div>
-                    <div class="BeastMaterialModal___content-list___22L6F">
-                        <div class="BeastMaterialModal___folder-list___2N-HN">
-                            <div class="BeastMaterialModal___folder-list-container___XPr0E">
-                                <div class="Tree_treeWrapper_4-64-0 Tree_small_4-64-0" data-testid="beast-core-tree">
-                                    <div>
-                                        <div class="Tree_treeNode_4-64-0 Tree_treeSelected_4-64-0 Tree_block_4-64-0 Tree_clickable_4-64-0" data-testid="beast-core-tree-treeNode" style="padding-left: 0px;">
-                                            <span class="Tree_iconWrapper_4-64-0 Tree_pointerable_4-64-0">
-                                                <i data-testid="beast-core-icon-caret-right" class="ICN_outerWrapper_4-64-0 ICN_type-caret-right_4-64-0  Tree_iconArrow_4-64-0 Tree_expand_4-64-0">
-                                                </i>
-                                            </span>
-                                            <div class="Tree_nodeContentWrapper_4-64-0 Tree_treeSelected_4-64-0 Tree_block_4-64-0 Tree_clickable_4-64-0">
-                                                <div class="Tree_labelWrapper_4-64-0">
-                                                    全部文件
-                                                </div>
-                                            </div>
+                    <div class="sf-upload-body">
+                        <div class="sf-upload-folder">
+                            <div class="sf-upload-folder-list">
+                                <div>
+                                    <div class="tree-node tree-node-active">
+                                        <div class="node-icon">
+                                            <i class="fe fe-chevron-down"></i>
+                                        </div>
+                                        <div class="node-folder-name">
+                                            全部文件
                                         </div>
                                     </div>
-                                    <div>
-                                        <div class="Tree_treeNode_4-64-0 Tree_block_4-64-0 Tree_clickable_4-64-0" data-testid="beast-core-tree-treeNode" style="padding-left: 16px;">
-                                            <span class="Tree_iconWrapper_4-64-0 Tree_pointerable_4-64-0">
-                                                <i data-testid="beast-core-icon-caret-right" class="ICN_outerWrapper_4-64-0 ICN_type-caret-right_4-64-0  Tree_iconArrow_4-64-0">
-                                                </i>
-                                            </span>
-                                            <div class="Tree_nodeContentWrapper_4-64-0 Tree_block_4-64-0 Tree_clickable_4-64-0">
-                                                <div class="Tree_labelWrapper_4-64-0">
-                                                    1
-                                                </div>
-                                            </div>
+                                </div>
+                                <div>
+                                    <div class="tree-node tree-node-1">
+                                        <div class="node-icon">
+                                            <i class="fe fe-chevron-down"></i>
+                                        </div>
+                                        <div class="node-folder-name">
+                                            1
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="BeastMaterialModal___content-right___ULxiI">
-                            <div class="BeastMaterialModal___list___Zq5yT">
-                                <div style="text-align: center; height: 100%; background: rgb(249, 249, 249); line-height: 340px;">
-                                    该分组暂无可用的图片
-                                </div>
-                            </div>
-                            <div class="BeastMaterialModal___content-bottom___1L3_G">
-                                <div class="BeastMaterialModal___tip___1Kkn2">
-                                    列表仅展示上传成功的图片
-                                </div>
-                                <div class="BeastMaterialModal___page___1uqix">
-                                    <ul class="PGT_outerWrapper_4-64-0">
-                                        <li class="PGT_totalText_4-64-0">
-                                            共有 0 条
-                                        </li>
-                                    </ul>
-                                </div>
+                        <div class="sf-upload-images">
+                            <div class="sf-upload-attach-list">
+                                <div class="none text-muted"><small>该分组暂无可用的图片</small></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <!-- <div class="modal-footer">
+                <button type="button" class="sf-btn sf-btn-primary">确认</button>
+                <button type="button" class="sf-btn btn-secondary" data-dismiss="modal">取消</button>
+            </div> -->
         </div>
     </div>
 </div>
-
-
 
 
 @endsection
